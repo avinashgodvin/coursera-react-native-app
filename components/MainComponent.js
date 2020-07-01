3715f5cf52aa191f701a5423dd38d8e804e634ce
avinash <avinashgodvin@.com>
2020-07-01 18:48:43 +0530

    after adding plates , navigation part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -25,25 +25,12 @@ const MenuNavigator = createStackNavigator({
 
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES , 
-      selectedDish: null
-    };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({selectedDish: dishId})
-}
 
   render() {
  
     return (
-        <View style={{flex: 1}}>
-            <Menu dishes={this.state.dishes} 
-            onPress={(dishId) => this.onDishSelect(dishId)}  />
-            <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> 
+        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+            <MenuNavigator />
         </View>
     );
     
